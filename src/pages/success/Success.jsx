8b20555f38dd8e8f8088ecfdafcbc3823cfa8279
Md Hussain <hussainakhtar1111@gmail.com>
2024-05-05 03:45:00 +0530

    Success page

--- src/pages/success/Success.jsx
+++ src/pages/success/Success.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import Button from "../../components/Button/Button";
 
 const Success = () => {
   return (
-    <div>Success</div>
-  )
-}
+    <div className="flex justify-between items-center">
+      <div>
+        <h3>Thank you</h3>
+        <p>Your details are saved successfully.</p>
+      </div>
+      <div>
+        <Button>Submit Again</Button>
+      </div>
+    </div>
+  );
+};
 
-export default Success
+export default Success;
